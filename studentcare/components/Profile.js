64a977a2636ed37015e79504077e2170133d2910
Udara Nalawansa <udarakavishka13@gmail.com>
2025-01-04 23:30:36 +0530

    feat: Update Profile component to use route parameters for user data display

--- studentcare/components/Profile.js
+++ studentcare/components/Profile.js
@@ -2,29 +2,27 @@ import React from 'react';
 import { View, StyleSheet, Image } from 'react-native';
 
 import { Text } from 'react-native-paper';
-import { students } from '../data/StudentsDb';
 import BottomTabNavigatior from './BottomTabNavigator'
-
-export default function Profile({ studentId }) {
-    const selectedStudent = students.find(student => student.id === studentId);
+export default function Profile({route}) {
+    const user = route.params;
     return (
         <>
         <BottomTabNavigatior/>
             <View style={styles.container}>
-                <Image style={styles.avatar} source={require('../assets/profilepic/1.jpg')} />
+                <Image style={styles.avatar} source={user.profile_pic} />
 
-                <Text variant='displayMedium'>{selectedStudent.name}</Text>
-                <Text> <Text style={styles.bold}> Age: </Text> {selectedStudent.age} </Text>
-                <Text> <Text style={styles.bold}> Gender: </Text> {selectedStudent.gender} </Text>
+                <Text variant='displayMedium'>{user.name}</Text>
+                <Text> <Text style={styles.bold}> Age: </Text> {user.age} </Text>
+                <Text> <Text style={styles.bold}> Gender: </Text> {user.gender} </Text>
                 <Text style={styles.bold}>Contact Information</Text>
-                <Text> <Text style={styles.bold}> Email: </Text> {selectedStudent.email} </Text>
-                <Text> <Text style={styles.bold}> Phone: </Text> {selectedStudent.phone} </Text>
-                <Text> <Text style={styles.bold}> Address: </Text> {selectedStudent.address} </Text>
+                <Text> <Text style={styles.bold}> Email: </Text> {user.email} </Text>
+                <Text> <Text style={styles.bold}> Phone: </Text> {user.phone} </Text>
+                <Text> <Text style={styles.bold}> Address: </Text> {user.address} </Text>
 
                 <Text style={styles.bold}>Biological Information</Text>
-                <Text> <Text style={styles.bold}> Gender: </Text> {selectedStudent.gender} </Text>
-                <Text> <Text style={styles.bold}> Age: </Text> {selectedStudent.age} </Text>
-                <Text> <Text style={styles.bold}> Blood Group: </Text> {selectedStudent.blood_group} </Text>
+                <Text> <Text style={styles.bold}> Gender: </Text> {user.gender} </Text>
+                <Text> <Text style={styles.bold}> Age: </Text> {user.age} </Text>
+                <Text> <Text style={styles.bold}> Blood Group: </Text> {user.blood_group} </Text>
             </View>
         </>
     );
