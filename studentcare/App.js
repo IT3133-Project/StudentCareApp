5ec4da53271afc7c5676ac9dd62db76cf37f36bf
Udara Nalawansa <udarakavishka13@gmail.com>
2025-01-04 23:45:00 +0530

    feat: Add navigation for Profile, Course, and Subjects components

--- studentcare/App.js
+++ studentcare/App.js
@@ -5,6 +5,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Course from './components/Course';
+import Subjects from './components/Subjects';
+import Profile from './components/Profile';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +17,9 @@ function RootStack() {
       headerStyle: { textAlign: 'center' },
     }}>
       <Stack.Screen name="Login" component={Login}   options={{ title: 'UoV Student Care' } }/>
+      <Stack.Screen name="Profile" component={Profile}   options={{ title: 'Profile' } }/>
+      <Stack.Screen name="Course" component={Course}   options={{ title: 'Course' } }/>
+      <Stack.Screen name="Subjects" component={Subjects}   options={{ title: 'Subjects' } }/>
     </Stack.Navigator>
   );
 }
