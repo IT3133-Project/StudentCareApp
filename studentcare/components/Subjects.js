2d7e81433d7d7713b54a199bace3470e0af75b18
Udara Nalawansa <udarakavishka13@gmail.com>
2025-01-04 21:16:57 +0530

    feat: Enhance Subjects component to display selected student information and integrate BottomTabNavigator

--- studentcare/components/Subjects.js
+++ studentcare/components/Subjects.js
@@ -1,10 +1,45 @@
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
 
-export default function Subjects(){
-    return(
+import { Text } from 'react-native-paper';
+import { courses } from '../data/StudentsDb';
+import BottomTabNavigatior from './BottomTabNavigator'
+
+export default function Subjects({ studentId }) {
+    const selectedStudent = courses.find(course => course.id ===studentId );
+    return (
         <>
-            <View>
-                <Text>Subjects</Text>
+        <BottomTabNavigatior/>
+            <View style={styles.container}>
+                <Text variant='displayMedium'>{selectedStudent.name}</Text>
+                <Text> <Text style={styles.bold}> Code: </Text> {selectedStudent.course_code} </Text>
+                <Text> <Text style={styles.bold}> Dept: </Text> {selectedStudent.department} </Text>
+                <Text style={styles.bold}>Course Information</Text>
+                <Text> <Text style={styles.bold}> Code: </Text> {selectedStudent.course_code} </Text>
+                <Text> <Text style={styles.bold}> Department: </Text> {selectedStudent.department} </Text>
+                <Text> <Text style={styles.bold}> Duration: </Text> {selectedStudent.duration} </Text>
+                <Text> <Text style={styles.bold}> Description: </Text> {selectedStudent.description} </Text>
             </View>
         </>
     );
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    avatar: {
+
+    },
+    info: {
+        fontSize: 16,
+        marginBottom: 5,
+    },
+    bold: {
+        fontWeight: 'bold',
+    }
+});
