b89d91268c1636444344d2d668f26313a00dcfe6
Udara Nalawansa <udarakavishka13@gmail.com>
2025-01-04 23:30:45 +0530

    feat: Enhance Course component to display course details using route parameters

--- studentcare/components/Course.js
+++ studentcare/components/Course.js
@@ -1,9 +1,47 @@
-export default function Course(){
-    return(
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+
+import { Text } from 'react-native-paper';
+import { courses } from '../data/StudentsDb';
+import BottomTabNavigatior from './BottomTabNavigator'
+
+export default function Course() {
+    const user = route.params;
+    const CourseDetail= courses.find( course=>user.course_id === course.id);
+
+    return (
         <>
-            <View>
-                <Text>Course</Text>
+        <BottomTabNavigatior/>
+            <View style={styles.container}>
+                <Text variant='displayMedium'>{CourseDetail.name}</Text>
+                <Text> <Text style={styles.bold}> Code: </Text> {CourseDetail.course_code} </Text>
+                <Text> <Text style={styles.bold}> Dept: </Text> {CourseDetail.department} </Text>
+                <Text style={styles.bold}>Course Information</Text>
+                <Text> <Text style={styles.bold}> Code: </Text> {CourseDetail.course_code} </Text>
+                <Text> <Text style={styles.bold}> Department: </Text> {CourseDetail.department} </Text>
+                <Text> <Text style={styles.bold}> Duration: </Text> {CourseDetail.duration} </Text>
+                <Text> <Text style={styles.bold}> Description: </Text> {CourseDetail.description} </Text>
             </View>
         </>
     );
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    avatar: {
+
+    },
+    info: {
+        fontSize: 16,
+        marginBottom: 5,
+    },
+    bold: {
+        fontWeight: 'bold',
+    }
+});
