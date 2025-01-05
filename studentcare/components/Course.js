6ebd80e40ef5ebf53c2864e63e0c8ca25f8248b5
Udara Nalawansa <udarakavishka13@gmail.com>
2025-01-05 18:05:54 +0530

    refactor: Refactor Course component to use Card for layout and improve styling with Header

--- studentcare/components/Course.js
+++ studentcare/components/Course.js
@@ -1,29 +1,49 @@
 import React from 'react';
 import { View, StyleSheet, Image } from 'react-native';
 
-import { Divider, Text } from 'react-native-paper';
+import { Card, Divider, Text } from 'react-native-paper';
 import { courses } from '../data/StudentsDb';
-import BottomTabNavigatior from './BottomTabNavigator'
+import Header from './Header';
 
-export default function Course() {
-    const user = route.params;
-    const CourseDetail= courses.find( course=>user.course_id === course.id);
+export default function Course({ route }) {
+    const user = route.params?.user;
+    const CourseDetail = courses.find(course => user.course_id === course.id);
 
     return (
         <>
-        <BottomTabNavigatior/>
-            <View style={styles.container}>
-                <Text variant='displayMedium'>{CourseDetail.name}</Text>
-                <Text> <Text style={styles.bold}> Code: </Text> {CourseDetail.course_code} </Text>
-                <Text> <Text style={styles.bold}> Dept: </Text> {CourseDetail.department} </Text>
-                <Divider/>
-                <Text style={styles.bold}>Course Information</Text>
-                <Text> <Text style={styles.bold}> Code: </Text> {CourseDetail.course_code} </Text>
-                <Text> <Text style={styles.bold}> Department: </Text> {CourseDetail.department} </Text>
-                <Text> <Text style={styles.bold}> Duration: </Text> {CourseDetail.duration} </Text>
-                <Text> <Text style={styles.bold}> Description: </Text> {CourseDetail.description} </Text>
-                <Divider/>
+
+
+            
+            <Header />
+
+            <View styles={styles.body}>
+                <Card style={styles.card}>
+                    <Card.Content>
+                        <View style={styles.container}>
+                            <View style={styles.overview}>
+                                <Text variant='displaySmall'>{CourseDetail.name}</Text>
+                                <Text>Code: {CourseDetail.course_code} | Dept: {CourseDetail.department} </Text>
+                            </View>
+                            <Divider />
+                            <Text style={styles.bold}>Course Information</Text>
+                            <Text>  Code:  {CourseDetail.course_code} </Text>
+                            <Text>  Department:  {CourseDetail.department} </Text>
+                            <Text>  Duration: {CourseDetail.duration} </Text>
+                            <Text>  Description:  {CourseDetail.description} </Text>
+                            <Divider />
+                        </View>
+                    </Card.Content>
+                </Card>
             </View>
+
+            <View style={styles.footer}>
+                <Text style={styles.footerText}>MyApp © 2024</Text>
+            </View>
+
+
+
+
+
 
         </>
     );
@@ -33,16 +53,21 @@ export default function Course() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        flexDirection: 'column',
+        marginTop: 5,
     },
-    avatar: {
-
+    card: {
+        marginTop: 20
     },
     info: {
         fontSize: 16,
         marginBottom: 5,
+    },
+    overview: {
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20
     },
     bold: {
         fontWeight: 'bold',
@@ -62,6 +87,12 @@ const styles = StyleSheet.create({
         flex: 2,
         width: "100%",
         alignItems: "center",
-        marginTop: 10,
+        marginTop: 20,
+        backgroundColor: '#4a148c',
+        height: 20,
     },
+    footerText: {
+        color: '#fff',
+    }
+
 });
