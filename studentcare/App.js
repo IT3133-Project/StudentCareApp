acedfbccfd1ea2ab9fe32974aa2ddc6f5557bfc8
Udara Nalawansa <udarakavishka13@gmail.com>
2025-01-05 14:37:31 +0530

    feat: Implement bottom tab navigation for Profile, Course, and Subjects components

--- studentcare/App.js
+++ studentcare/App.js
@@ -1,54 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Login from './components/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PaperProvider } from 'react-native-paper';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Course from './components/Course';
 import Subjects from './components/Subjects';
 import Profile from './components/Profile';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function RootStack() {
   return (
-    <Stack.Navigator  initialRouteName="Login" screenOptions={{
-      headerStyle: { textAlign: 'center' },
-    }}>
-      <Stack.Screen name="Login" component={Login}   options={{ title: 'UoV Student Care' } }/>
-      <Stack.Screen name="Profile" component={Profile}   options={{ title: 'Profile' } }/>
-      <Stack.Screen name="Course" component={Course}   options={{ title: 'Course' } }/>
-      <Stack.Screen name="Subjects" component={Subjects}   options={{ title: 'Subjects' } }/>
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: { backgroundColor: '#f5f5f5' },
+        headerTitleStyle: { fontWeight: 'bold' },
+      }}
+    >
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ title: 'UoV Student Care' }}
+      />
+      <Stack.Screen
+        name="MainTab"
+        component={MainTab}
+        options={{ title: 'UoV Student Care' }}
+      />
     </Stack.Navigator>
   );
 }
 
-
+function MainTab({ route }) {
+  const student = route.params;
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#4a148c",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: { backgroundColor: "white" },
+        headerShown: false,
+      }}>
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        initialParams={student}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="account" size={size} color={color} />;
+          },
+        }} />
+      <Tab.Screen
+        name="Course"
+        component={Course}
+        initialParams={student}
+        options={{
+          tabBarLabel: 'Course',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="school" size={size} color={color} />;
+          },
+        }} />
+      <Tab.Screen
+        name="Subjects"
+        component={Subjects}
+        initialParams={student}
+        options={{
+          tabBarLabel: 'Subjects',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="book-open-outline" size={size} color={color} />;
+          },
+        }} />
+    </Tab.Navigator>
+  );
+}
 export default function App() {
   return (
     <PaperProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={styles.container}>
-          <NavigationContainer>
-            <RootStack />
-          </NavigationContainer>
-        </SafeAreaView>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <StatusBar style="auto" />
+          <RootStack />
+        </NavigationContainer>
       </SafeAreaProvider>
     </PaperProvider>
-
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  safeArea: {
     flex: 1,
     backgroundColor: '#fff',
   },
