a051e359d7b1bf493ca5f787604cbe281c5bb8c3
Udara Nalawansa <udarakavishka13@gmail.com>
2025-01-05 17:46:56 +0530

    fix: Add flex property to Header styles for improved layout

--- studentcare/components/Profile.js
+++ studentcare/components/Profile.js
@@ -1,33 +1,53 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, ScrollView } from 'react-native';
 
-import { Divider, Text } from 'react-native-paper';
+import { Avatar, Card, Divider, Text } from 'react-native-paper';
 import BottomTabNavigatior from './BottomTabNavigator'
-export default function Profile({route}) {
+import Header from './Header';
+export default function Profile({ route }) {
     const user = route.params?.user;
     return (
         <>
-        {/* <BottomTabNavigatior/> */}
-            <View style={styles.container}>
-                <Image style={styles.avatar} source={{uri:user.profile_pic}} />
+            <ScrollView>
+                <Header />
+                <Card>
+                    <Card.Content>
+                        <View style={styles.container}>
+                            <View style={styles.overview}>
+                                <Avatar.Image
+                                    size={150}
+                                    source={user.profile_pic}
+                                    style={styles.avatar}
+                                />
+                                <Text variant='displaySmall' >{user.name}</Text>
+                                <Text>  Age: {user.age} | Gender: {user.gender} </Text>
+                            </View>
+                            <Divider />
 
-                <Text variant='displayMedium'>{user.name}</Text>
-                <Text> <Text style={styles.bold}> Age: </Text> {user.age} | </Text>
-                <Text> <Text style={styles.bold}> Gender: </Text> {user.gender} </Text>
-                <Divider/>
-                <Text style={styles.bold}>Contact Information</Text>
-                <Text> <Text style={styles.bold}> Email: </Text> {user.email} </Text>
-                <Text> <Text style={styles.bold}> Phone: </Text> {user.phone} </Text>
-                <Text> <Text style={styles.bold}> Address: </Text> {user.address} </Text>
-                <Divider/>
-                <Text style={styles.bold}>Biological Information</Text>
-                <Text> <Text style={styles.bold}> Gender: </Text> {user.gender} </Text>
-                <Text> <Text style={styles.bold}> Age: </Text> {user.age} </Text>
-                <Text> <Text style={styles.bold}> Blood Group: </Text> {user.blood_group} </Text>
-            </View>
-            <View style={styles.footer}>
-                <Text>MyApp © 2024</Text>
-            </View>
+                            <View style={styles.info}>
+                                <Text style={styles.bold}>Contact Information</Text>
+                                <Text> Email: {user.email} </Text>
+                                <Text> Phone:  {user.phone} </Text>
+                                <Text> Address: {user.address} </Text>
+                            </View>
+
+                            <Divider />
+
+                            <View style={styles.bioInfo}>
+                                <Text style={styles.bold}>Biological Information</Text>
+                                <Text> Gender:  {user.gender} </Text>
+                                <Text> Age:  {user.age} </Text>
+                                <Text> Blood Group:  {user.blood_group} </Text>
+                            </View>
+                        </View>
+                    </Card.Content>
+                </Card>
+
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>MyApp © 2024</Text>
+                </View>
+            </ScrollView>
+
         </>
     );
 }
@@ -36,16 +56,26 @@ export default function Profile({route}) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        marginTop: 10,
+        flexDirection: 'column',
     },
     avatar: {
+        marginBottom: 10,
 
     },
+    overview: {
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 50
+    },
     info: {
-        fontSize: 16,
-        marginBottom: 5,
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    bioInfo: {
+        marginTop: 10,
     },
     bold: {
         fontWeight: 'bold',
@@ -66,5 +96,11 @@ const styles = StyleSheet.create({
         width: "100%",
         alignItems: "center",
         marginTop: 10,
+        backgroundColor: '#4a148c',
+        height:30
     },
+    footerText:{
+        color: '#fff',
+    }
+    
 });
